428fef59950d5fbf362e9ca1e3db774595e50e6e
JBlemy <jblemy2002@gmail.com>
2024-06-14 13:12:56 -0400

    Modification header

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
+const button = document.getElementById("button-menu");
+const links = document.getElementById("links");
+const links_icon = document.querySelector(".button-menu i")
 
-function affichermenu()
-{
-    if (document.getElementById("menu").style.display == "block")
-    {
-        document.getElementById("menu").style.display="none";
-    }
-    else
-    {
-        document.getElementById("menu").style.display="block";
+function view_links(){
+    if(links.classList.contains("view")){
+        links.classList.remove("view");
+        links_icon.classList.add("fa-bars")
+        links_icon.classList.remove("fa-times")
+    }else{
+        links.classList.add("view");
+        links_icon.classList.remove("fa-bars")
+        links_icon.classList.add("fa-times")
     }
 }
